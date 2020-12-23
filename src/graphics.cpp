a177a4059352d8d82002bdce10d1093a01151aea
lukasobg <lukasobg@users.noreply.github.com>
2020-12-23 18:02:33 +0200

    Mainly adding user input features.

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,6 +1,6 @@
 #include "graphics.hpp"
 
-
+/* Destructor. */
 Graphics::~Graphics() {
 	for (auto g_cell : g_cells_ ) {
 		delete g_cell;
@@ -8,6 +8,8 @@ Graphics::~Graphics() {
 	delete world_;
 }
 
+/* Initialize is called in the constructor. It creates a window, initializes the world,
+   and randomly sets some cells alive. */
 void Graphics::initialize() {
 	// Create window
 	window_.create(sf::VideoMode(w_, h_), "Game of Life");
@@ -17,17 +19,8 @@ void Graphics::initialize() {
 	width_ = 0.9*w_;
 	height_ = 0.9*h_;
 
-	// Draw border
-	/*
-	border_ = new sf::RectangleShape();
-	border_->setSize(sf::Vector2f(width_,height_));
-	border_->setPosition(sf::Vector2f((w_-width_) / 2.0, (h_-height_) / 2.0));
-	border_->setOutlineThickness(1.f);
-	border_->setOutlineColor(sf::Color::White);
-	*/
-
 	// Initialize world
-	world_ = new World(30,30);
+	world_ = new World(40,40);
 	int nx = world_->get_nx();
 	int ny = world_->get_ny();
 
@@ -37,24 +30,39 @@ void Graphics::initialize() {
 			add_cell(i,j);
 		}
 	}
-	
-	// Randomly set some cells alive
-	srand(time(0));
-	int x; int y;
-	for (int i = 0; i< (nx*ny)/5; i++) {
-	  	std::uniform_real_distribution<double> u(0,1);
-	  	x = (rand() % nx);
-	  	y = (rand() % ny);
-	  	world_->set_alive(x, y);
+
+	// Initialize empty configurations
+	for (int i = 0; i<9; i++) {
+		std::vector<int> v = {};
+		cells_alive_.push_back(v);
 	}
 	
-	
-	
-	// GLIDER GUN
-	//world_->add_glider_gun(1,1);
-	//world_->add_glider_gun(1,8);
+	init_default_configurations();
+
+	add_random_cells((nx*ny)/6);
 }
 
+// Randomly set some cells alive
+void Graphics::add_random_cells(int nr) {
+	srand(time(0));
+	int x; int y;
+	for (int i = 0; i<nr; i++) {
+	  	std::uniform_real_distribution<double> u(0,1);
+	  	x = (rand() % world_->get_nx());
+	  	y = (rand() % world_->get_ny());
+	  	world_->set_alive(x, y);
+	}
+}
+
+/* Clears the world, i.e. kills all cells. */ 
+void Graphics::clear() {
+	paused_ = true;
+
+	for (auto cell : world_->get_cells()) {
+		cell->set_dead();
+	}
+}
+/* Adds a cell to the world and creates a graphical cell. */
 void Graphics::add_cell(int i, int j) {
 	Cell* cell = new Cell(i, j);
 	world_->insert(cell);
@@ -64,12 +72,50 @@ void Graphics::add_cell(int i, int j) {
 	g_cell->setSize(g_size());
 	g_cell->setPosition(g_pos(i,j));
 	g_cell->setFillColor(sf::Color::Black);
+	g_cell->setOutlineColor(sf::Color::Black);
+	g_cell->setOutlineThickness(1.5);
 	g_cells_.push_back(g_cell);
 }
 
+void Graphics::save_configuration(int nr) {
+	cells_alive_[nr].clear();
+	for (auto cell : world_->get_cells() ) {
+		if (cell->is_alive()) {
+			cells_alive_[nr].push_back(1);
+		} else {
+			cells_alive_[nr].push_back(0);
+		}
+	}
 
+	std::cout << "Saved configuration " << nr+1 << "." << std::endl;
+}
+
+void Graphics::load_configuration(int nr) {
+
+	if (cells_alive_[nr].size() < 2) {
+		std::cout << "No configuraion: " << nr+1 << std::endl;
+	} else {
+
+		paused_ = true;
+
+		int i = 0;
+		for (auto cell : world_->get_cells() ) {
+			if (cells_alive_[nr][i] == 1) {
+				cell->set_alive();
+			} else {
+				cell->set_dead();
+			}
+			i++;
+		}
+
+		std::cout << "Loading configuration " << nr+1 << "." << std::endl;
+	}
+}
+
+/* This function contains the loop that is run as long as the 
+   window is open. Calls user input parsing and drawing functions.
+   Also takes care of the speed of the simulation, based on realtime. */ 
 void Graphics::animate(void) {
-	std::cout << "Animating..." << std::endl;
 
 	// initial draw
 	redraw();
@@ -92,6 +138,7 @@ void Graphics::animate(void) {
 	}
 }
 
+/* Parses user input. Check readme for clear instructions. */
 void Graphics::parse_user_input() {
     sf::Event event;
     while (window_.pollEvent(event)) {
@@ -113,6 +160,34 @@ void Graphics::parse_user_input() {
           	else if (event.key.code == sf::Keyboard::Down) {
           		toggle_speed(0);
           	}
+
+          	else if (event.key.code == sf::Keyboard::Right) {
+          		if (paused_) {
+          			world_->update();
+          		}
+          	}
+
+          	else if (event.key.code == sf::Keyboard::C) {
+          		clear();
+          	}
+
+          	else if (event.key.code == sf::Keyboard::R) {
+          		paused_ = true;
+          		clear();
+          		add_random_cells((world_->get_ny()*world_->get_ny())/6);
+          	}
+
+          	else if ( event.key.shift && event.key.code > sf::Keyboard::Num0 && event.key.code <= sf::Keyboard::Num9 ) {
+          		if (paused_) save_configuration(event.key.code-sf::Keyboard::Num0-1);
+          	}
+
+          	else if (event.key.code > sf::Keyboard::Num0 && event.key.code <= sf::Keyboard::Num9 ) {
+          		load_configuration(event.key.code-sf::Keyboard::Num0-1);
+          	}
+
+          	else if (event.key.code == sf::Keyboard::Q) {
+          		window_.close();
+          	}
         }
 
         if (event.type == sf::Event::MouseButtonPressed) {
@@ -124,13 +199,13 @@ void Graphics::parse_user_input() {
         }
     }
 }
-
+/* Toggles, i.e. kills a live cell,  */
 void Graphics::toggle_cell_at(int click_x, int click_y) {
 	int idx = 0;
 	for (auto g_cell : g_cells_) {
-		auto bounds = g_cell->getLocalBounds();
-		if (bounds.contains(sf::Vector2f(click_x,click_y))) {
-			std::cout << "Click in bounds:";
+		auto bounds = g_cell->getGlobalBounds();
+
+		if (bounds.contains(click_x,click_y)) {
 			if (!world_->is_alive(idx)) {
 				world_->set_alive(idx);
 				redraw();
@@ -149,15 +224,15 @@ sf::Vector2f Graphics::g_pos(int x, int y) const {
 	double x_start = (w_-width_) / 2;
 	double y_start = (h_-height_) / 2;
 
-	double gx = x_start + unit_x * x + 5;
-	double gy = y_start + unit_y * y + 5;
+	double gx = x_start + unit_x * x + 3 ;
+	double gy = y_start + unit_y * y + 3 ;
 
 	return sf::Vector2f(gy, gx);
 }
 
 sf::Vector2f Graphics::g_size() const {
-	double unit_x = ( width_ / world_->get_nx() ) - 5;
-	double unit_y = ( height_ / world_->get_ny() ) - 5;
+	double unit_x = ( width_ / world_->get_nx() ) - 3;
+	double unit_y = ( height_ / world_->get_ny() ) - 3;
 	return sf::Vector2f(unit_x, unit_y);
 }
 
@@ -169,22 +244,9 @@ void Graphics::redraw() {
 	}
 
 	window_.clear();
-
-	// Track speed, draws font in lower right corner
-	sf::Font font;
-  	font.loadFromFile("../resources/arial.ttf");
-    sf::Text msg;
-    msg.setFont(font);
-    msg.setCharacterSize(16);
-    msg.setFillColor(sf::Color::White);
-    msg.setPosition(w_-150, h_-20);
-    std::stringstream ss;
-    double percentage = (1 - (step_length_ - 20000.0) / (20000.0+500000.0)) * 100;
-    ss << "Speed: " << std::setprecision(1) << std::fixed << percentage << "%.";
-    std::string s = ss.str();
-    msg.setString(s);
-    window_.draw(msg);
 	
+	draw_texts();
+
 	// Redraw cells
 	int i = 0;
 	for ( auto cell : world_->get_cells() ) {
@@ -198,11 +260,40 @@ void Graphics::redraw() {
 		}
 		i++;
 	}
+
 	//window_.draw(*border_);
 
 
 	window_.display();
 	
+}
+
+void Graphics::draw_texts() {
+	// Load font
+	sf::Font font;
+  	font.loadFromFile("../resources/arial.ttf");
+  	// Track speed, draws font in lower right corner
+    sf::Text msg;
+    msg.setFont(font);
+    msg.setCharacterSize(16);
+    msg.setFillColor(sf::Color::White);
+    msg.setPosition(w_-150, h_-20);
+    std::stringstream ss;
+    double percentage = (1 - (step_length_ - 20000.0) / (20000.0+500000.0)) * 100;
+    ss << "Speed: " << std::setprecision(1) << std::fixed << percentage << "%.";
+    std::string s = ss.str();
+    msg.setString(s);
+    window_.draw(msg);
+    
+    // Paused
+    sf::Text msg2;
+    msg2.setFont(font);
+    msg2.setCharacterSize(16);
+    msg2.setFillColor(sf::Color::White);
+    msg2.setPosition(w_-260, h_-20);
+    std::string s2 = "PAUSED";
+    msg2.setString(s2);
+    if (paused_) window_.draw(msg2);
 }
 
 void Graphics::toggle_speed(int i) {
@@ -213,6 +304,7 @@ void Graphics::toggle_speed(int i) {
 	}
 }
 
-void Graphics::quit() {
-	
+
+void Graphics::init_default_configurations() {
+
 }
--- src/graphics.hpp
+++ src/graphics.hpp
@@ -13,6 +13,8 @@
 
 class World;
 
+
+/* Graphics class. This class contains the graphics for our simulation. */
 class Graphics {
 private:
 	sf::RenderWindow window_;
@@ -26,21 +28,27 @@ private:
 
 	bool paused_ = true;
 	double step_length_ = 100000.0;
+	std::vector<std::vector<int>> cells_alive_;
 
 	sf::Vector2f g_pos(int x, int y) const;
 	sf::Vector2f g_size() const;
 public:
 
-	Graphics() : w_(700), h_(700) { initialize(); }
+	Graphics() : w_(800), h_(800) { initialize(); }
 	~Graphics();
 
 	void initialize();
 	void animate();
 	void parse_user_input();
 	void redraw();
-	void quit();
+	void draw_texts();
 
+	void add_random_cells(int nr);
+	void clear();
 	void add_cell(int i, int j);
 	void toggle_cell_at(int click_x, int click_y);
 	void toggle_speed(int i);
+	void save_configuration(int nr);
+    void load_configuration(int nr);
+    void init_default_configurations();
 };
